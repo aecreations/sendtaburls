1590648535d8c25b2d2d2736f40e7bd57e6ddd60
AE Creations <aecreations@server.fake>
2014-06-14 23:28:57 -0700

    Added methods getPref() and setPref() to automatically set and retrieve prefs from the "extensions.aecreations" pref branch

--- src/modules/aeUtils.js
+++ src/modules/aeUtils.js
@@ -1,8 +1,38 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Send Tab URLs.
+ *
+ * The Initial Developer of the Original Code is 
+ * Alex Eng <ateng@users.sourceforge.net>.
+ * Portions created by the Initial Developer are Copyright (C) 2008-2014
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 const EXPORTED_SYMBOLS = ["aeUtils"];
 
-const DEBUG = false;
+const DEBUG = true;
+const PREFNAME_PREFIX = "extensions.aecreations.";
 const EXTENSION_ID = "{4aebcd37-f454-4928-9233-174a026ed367}";
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+var Application = Cc["@mozilla.org/fuel/application;1"].getService(Ci.fuelIApplication);
 
 
 var aeUtils; 
@@ -63,6 +93,20 @@ aeUtils.getOS = function ()
   return rv;
 };
 
+
+aeUtils.getPref = function (aPrefKey, aDefaultValue)
+{
+  // aPrefKey is the pref name, but without the "extensions.aecreations."
+  // prefix.
+  return Application.prefs.getValue(PREFNAME_PREFIX + aPrefKey, aDefaultValue);
+};
+
+
+aeUtils.setPref = function (aPrefKey, aPrefValue)
+{
+  Application.prefs.setValue(PREFNAME_PREFIX + aPrefKey, aPrefValue);
+},
+  
 
 aeUtils.beep = function () 
 {
