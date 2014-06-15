46ef17c53beedf97eb384646dfd0db8b803fc0a0
AE Creations <aecreations@server.fake>
2014-06-14 23:30:12 -0700

    Migrate prefs to the "extensions.aecreations" pref branch

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -1,12 +1,14 @@
-pref("sendtabs.message.format.list_style", 0);
-pref("sendtabs.message.format.always_ask", true);
-pref("sendtabs.mailclient", 0);
-pref("sendtabs.mailclient.googleapps.enabled", false);
-pref("sendtabs.mailclient.googleapps.domain", "");
-pref("sendtabs.mailclient.clipboard.url_count_position", 1);
-pref("sendtabs.mailclient.clipboard.confirm_copy", true);
-pref("sendtabs.webmail.open_in_new_window", false);
-pref("sendtabs.webmail.window_features", "");
-pref("sendtabs.show_tab_menu_command", true);
-pref("sendtabs.reload_pending_tabs", true);
 pref("extensions.{4aebcd37-f454-4928-9233-174a026ed367}.description", "chrome://sendtabs/locale/sendtabs.properties");
+
+pref("extensions.aecreations.sendtabs.message.format.list_style", 0);
+pref("extensions.aecreations.sendtabs.message.format.always_ask", true);
+pref("extensions.aecreations.sendtabs.mailclient", 0);
+pref("extensions.aecreations.sendtabs.mailclient.googleapps.enabled", false);
+pref("extensions.aecreations.sendtabs.mailclient.googleapps.domain", "");
+pref("extensions.aecreations.sendtabs.mailclient.clipboard.url_count_position", 1);
+pref("extensions.aecreations.sendtabs.mailclient.clipboard.confirm_copy", true);
+pref("extensions.aecreations.sendtabs.webmail.open_in_new_window", false);
+pref("extensions.aecreations.sendtabs.webmail.window_features", "");
+pref("extensions.aecreations.sendtabs.show_tab_menu_command", true);
+pref("extensions.aecreations.sendtabs.reload_pending_tabs", true);
+pref("extensions.aecreations.sendtabs.migrate_prefs", true);
