cfb79fb9d78e433b81b93b2906b002391658288f
AE Creations <aecreations@server.fake>
2014-06-15 22:26:51 -0700

    Removed duplicate pref entries

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -3,7 +3,7 @@ pref("extensions.{4aebcd37-f454-4928-9233-174a026ed367}.description", "chrome://
 pref("extensions.aecreations.sendtabs.message.format.list_style", 0);
 pref("extensions.aecreations.sendtabs.message.format.always_ask", true);
 pref("extensions.aecreations.sendtabs.mailclient", 0);
-pref("extensions.aecreations.sendtabs.mailclient.googleapps.enabled", false);
+pref("extensions.aecreations.sendtabs.mailclient.googleapps.enabled", true);
 pref("extensions.aecreations.sendtabs.mailclient.googleapps.domain", "");
 pref("extensions.aecreations.sendtabs.mailclient.clipboard.url_count_position", 1);
 pref("extensions.aecreations.sendtabs.mailclient.clipboard.confirm_copy", true);
@@ -11,6 +11,4 @@ pref("extensions.aecreations.sendtabs.webmail.open_in_new_window", false);
 pref("extensions.aecreations.sendtabs.webmail.window_features", "");
 pref("extensions.aecreations.sendtabs.show_tab_menu_command", true);
 pref("extensions.aecreations.sendtabs.reload_pending_tabs", true);
-pref("extensions.aecreations.sendtabs.mailclient.googleapps.enabled", true);
-pref("extensions.aecreations.sendtabs.mailclient.googleapps.domain", "");
 pref("extensions.aecreations.sendtabs.migrate_prefs", true);
